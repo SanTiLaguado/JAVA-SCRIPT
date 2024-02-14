4db3c9584c6e839135af1a0b986a351822e4d881
santilaguado <santiagolaguado57@gmail.com>
2024-02-14 17:21:53 -0500

    day 4

---
+++ subsecuencias de adn/script.js
@@ -0,0 +1,5 @@
+const buscar = () => {
+    const secuencia = document.getElementById("secuenciaIngresada").value;
+    const subsecuencia = document.getElementById("subsecuenciaIngresada").value;
+} 
+
